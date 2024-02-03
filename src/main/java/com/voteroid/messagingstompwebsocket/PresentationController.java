4566269aef0a5be42899da7010ea0f968a9259d7
mpn <maurice.naftalin@groupcdg.com>
2024-02-03 16:43:21 +0000

    Renamed example classes

--- src/main/java/com/voteroid/messagingstompwebsocket/PresentationController.java
+++ src/main/java/com/voteroid/messagingstompwebsocket/PresentationController.java
@@ -1,19 +1,18 @@
 package com.voteroid.messagingstompwebsocket;
 
-import com.voteroid.model.Model;
+import com.voteroid.model.Presentation;
 import com.voteroid.model.Slide;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @org.springframework.web.bind.annotation.RestController
-public class RestController {
+public class PresentationController {
 
-    @Autowired private Model model;
+    @Autowired private Presentation presentation;
     @Autowired private SimpMessagingTemplate simpTemplate;
 
     @PutMapping("/{question}/answers/{answerIndex}/vote")
@@ -26,25 +25,25 @@ public class RestController {
     @PostMapping("/slides")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void createSlide(@RequestBody Slide slide) {
-        model.addSlide(slide);
+        presentation.addSlide(slide);
     }
 
     @PutMapping("/current-question-name")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void setCurrentQuestionName(@RequestBody String currentQuestionName) {
-        model.setCurrentQuestionName(currentQuestionName);
+        presentation.setCurrentQuestionName(currentQuestionName);
         simpTemplate.convertAndSend("/topic/currentQuestion/", Map.of("content", currentQuestionName));
     }
 
     @GetMapping("/slides/current-slide")
     public Slide getCurrentSlide() {
-        return model.getCurrentSlide();
+        return presentation.getCurrentSlide();
     }
 
     // called by presentation iframe to set up
     @GetMapping("/slides/{questionName}")
     public Slide getSlide(@PathVariable String questionName) {
-        return model.getSlide(questionName);
+        return presentation.getSlide(questionName);
     }
 
     // alternative annotations (not sure of the difference)
--- src/main/java/com/voteroid/model/Presentation.java
+++ src/main/java/com/voteroid/model/Presentation.java
@@ -1,18 +1,16 @@
 package com.voteroid.model;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Model {
+public class Presentation {
 
     private final Map<String,Slide> slides;
     private String currentQuestionName;
     private final static String NO_QUESTION = "No question";
 
-    public Model() {
+    public Presentation() {
         slides = new ConcurrentHashMap<>();
         slides.put(NO_QUESTION, new Slide("No question", List.of()));
     }
