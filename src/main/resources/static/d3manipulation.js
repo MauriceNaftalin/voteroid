6f2488350a91858dbb9c52657520f2ce2c882af4
mpn <maurice.naftalin@groupcdg.com>
2024-02-27 09:07:29 +0000

    phoneClient adjusted for window size; iframe not working

--- src/main/resources/static/d3manipulation.js
+++ src/main/resources/static/d3manipulation.js
@@ -1,36 +1,39 @@
-var data = [0,0,0];
-const svg = d3.select("svg");
-const svgWidth = 250, svgHeight = 250;
-const barWidth = svgWidth / data.length;
+function setUpSvg(svgWidth, svgHeight) {
+    var data = [0, 0, 0];
+    const d3svg = d3.select("svg");
+    var barWidth = svgWidth / data.length;
 
-// Can't open a JS console, so use a label for diagnostic output
-function consoleOutput(text) {
-    document.getElementById('consoleLog').textContent = text;
+    d3svg.selectAll("rect")
+        .data(data)
+        .enter().append("rect")
+        .attr("x", (d, i) => i * barWidth)
+        .attr("y", d => svgHeight - d * 10) // Scale the bar height
+        .attr("width", barWidth - 1)
+        .attr("height", d => d * 10)
+        .attr("fill", "steelblue")
+    ;
+    return {d3svg, svgHeight, barWidth};
 }
 
-svg.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-    .attr("x", (d, i) => i * barWidth)
-    .attr("y", d => svgHeight - 20 - d * 10) // Scale the bar height
-    .attr("width", barWidth - 1)
-    .attr("height", d => d * 10)
-    .attr("fill", "steelblue");
+function showQuestionName(text) {
+    document.getElementById('questionName').textContent = text;
+}
 
 function setBarHeight(voteUpdate) {
     console.log('answer: ' + voteUpdate)
     const voteCount = voteUpdate.voteCount;
-    svg.selectAll(`rect:nth-child(${voteUpdate.answerIndex})`)
+    const d3svg = d3.select("svg");
+    d3svg.selectAll(`rect:nth-child(${voteUpdate.answerIndex})`)
         .attr("height", function (d) {
             return voteCount * 10;
         })
         .attr("y", function (d) {
-            return svgHeight - (voteCount * 10) - 20;
+            return chartHeight - (voteCount * 10);
         });
 }
 
 function setColumnLabels(labelsText) {
-    svg.selectAll("text")
+    d3svg.selectAll("text")
         .data(labelsText)
         .enter()
         .append("text")
